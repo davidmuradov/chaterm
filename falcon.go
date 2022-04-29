86c5d865f695438110fed2b915e2fda30a29492b
davidmuradov <david.linuxterminal@gmail.com>
2022-04-28 22:20:09 -0400

    setting up tview

--- falcon.go
+++ falcon.go
@@ -1,9 +1,12 @@
 package main
 
-import( "fmt"
+import( // "fmt"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	fmt.Println("falcon")
+	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
 }
