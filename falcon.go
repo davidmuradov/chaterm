05f2eefa49fecfa2f646fe5cf5bd04785fdb5ed2
davidmuradov <david.linuxterminal@gmail.com>
2022-05-02 18:08:23 -0400

    added simple layout

--- falcon.go
+++ falcon.go
@@ -5,8 +5,34 @@ import( // "fmt"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+
+	// function to define the title of grid elements
+	newPrimitive := func(title string) tview.Primitive {
+		return tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetText(title)
+	}
+
+	contacts := newPrimitive("Contacts")
+	messages := newPrimitive("Messages")
+
+	grid := tview.NewGrid().
+		SetRows(/* set height */0).
+		SetColumns(0).
+		SetBorders(true)
+
+	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
+	grid.AddItem(contacts, 0, 0, 0, 0, 0, 0, false).
+		AddItem(messages, 0, 0, 0, 0, 0, 0, false)
+
+	// Layout for screens wider than 100 cells.
+	grid.AddItem(contacts, 0, 0, 1, 1, 0, 100, false).
+		AddItem(messages, 0, 1, 1, 1, 0, 100, false)
+
+	app := tview.NewApplication().
+		SetRoot(grid, true).
+		Run()
+	if err := app; err != nil {
 		panic(err)
 	}
 }
