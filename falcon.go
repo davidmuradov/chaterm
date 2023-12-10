b9b830195ec8d079a50439033ae7618819a779ea
DanatAliMuradov <2357647@edu.vaniercollege.qc.ca>
2023-12-10 11:23:51 -0500

    test

--- falcon.go
+++ falcon.go
@@ -1,11 +1,13 @@
 package main
 
-import( // "fmt"
+import( 
 	"github.com/rivo/tview"
+    "fmt"
 )
 
 // Git test
 func main() {
+    fmt.Println("test")
 	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
 	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
 		panic(err)
