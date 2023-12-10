0478c2c7c6e74274bebebda3147f6c801fb5ff61
davidmuradov <david.linuxterminal@gmail.com>
2023-12-10 11:57:14 -0500

    test3

--- falcon.go
+++ falcon.go
@@ -1,11 +1,12 @@
 package main
 
-import( 
+import( "fmt"
 	"github.com/rivo/tview"
 )
 
 // Git test
 func main() {
+	fmt.Println("test")
 	newPrimitive := func(title string) tview.Primitive {
 		return tview.NewTextView().
 			SetTextAlign(tview.AlignCenter).
