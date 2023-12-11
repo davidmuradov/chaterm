9d172b2403ad32963fc9a19a787f3331676c10d7
davidmuradov <david.linuxterminal@gmail.com>
2023-12-11 12:26:33 -0500

    updates

--- falcon.go
+++ falcon.go
@@ -1,38 +1,42 @@
 package main
 
-import( "fmt"
+import (
+	//"fmt"
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-// Git test
+type Theme struct {
+	PrimitiveBackgroundColor    tcell.Color // Main background color for primitives.
+	ContrastBackgroundColor     tcell.Color // Background color for contrasting elements.
+	MoreContrastBackgroundColor tcell.Color // Background color for even more contrasting elements.
+	BorderColor                 tcell.Color // Box borders.
+	TitleColor                  tcell.Color // Box titles.
+	GraphicsColor               tcell.Color // Graphics.
+	PrimaryTextColor            tcell.Color // Primary text.
+	SecondaryTextColor          tcell.Color // Secondary text (e.g. labels).
+	TertiaryTextColor           tcell.Color // Tertiary text (e.g. subtitles, notes).
+	InverseTextColor            tcell.Color // Text on primary-colored backgrounds.
+	ContrastSecondaryTextColor  tcell.Color // Secondary text on ContrastBackgroundColor-colored backgrounds.
+}
+
 func main() {
-	fmt.Println("test")
-	newPrimitive := func(title string) tview.Primitive {
-		return tview.NewTextView().
-			SetTextAlign(tview.AlignCenter).
-			SetText(title)
-	}
+	app := tview.NewApplication()
 
-	contacts := newPrimitive("Contacts")
-	messages := newPrimitive("Messages")
+	// Login Form
+	loginForm := tview.NewForm().
+	AddInputField("XMPP email adress", "rat@404.city", 25, nil, nil).
+	AddPasswordField("Password", "", 25, 0, nil).
+	AddButton("Login", nil).
+	AddButton("Quit", func() {app.Stop()})
 
-	grid := tview.NewGrid().
-		SetRows(/* set height */0).
-		SetColumns(0).
-		SetBorders(true)
+	// Main application's structure
+	rootPrimitive := tview.NewPages().
+	AddPage("loginPage", loginForm, true, true)
 
-	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
-	grid.AddItem(contacts, 0, 0, 0, 0, 0, 0, false).
-		AddItem(messages, 0, 0, 0, 0, 0, 0, false)
-
-	// Layout for screens wider than 100 cells.
-	grid.AddItem(contacts, 0, 0, 1, 1, 0, 100, false).
-		AddItem(messages, 0, 1, 1, 1, 0, 100, false)
-
-	app := tview.NewApplication().
-		SetRoot(grid, true).
-		Run()
-	if err := app; err != nil {
+	// app needs to be of type tview.Primitive inside app.SetRoot()
+	// form item is a valid primitive
+	if err := app.SetRoot(rootPrimitive, true).SetFocus(rootPrimitive).Run(); err != nil {
 		panic(err)
 	}
 }
