fc77fdb329c779c8778004dcccc1b656dfb194a1
davidmuradov <david.linuxterminal@gmail.com>
2024-08-05 13:40:29 -0400

    Test

--- go.mod
+++ go.mod
@@ -1,17 +1,3 @@
-module github.com/rivo/tview/mod
+module github.com/davidmuradov/falcon/gui
 
-go 1.18
-
-require github.com/rivo/tview v0.0.0-20231206124440-5f078138442e
-
-require (
-	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell v1.4.0 // indirect
-	github.com/gdamore/tcell/v2 v2.7.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.4.4 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/term v0.15.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-)
+go 1.22.5
---
+++ gui/nord.go
@@ -0,0 +1,28 @@
+package main
+
+const (
+
+    // POLAR NIGHT
+    NORD0 = 0x2e3440 // dark
+    NORD1 = 0x3b4252 // brighter dark
+    NORD2 = 0x434c5e // brigther brighter dark
+    NORD3 = 0x4c566a // birghtest dark
+
+    // SNOW STORM
+    NORD4 = 0xd8dee9 // white
+    NORD5 = 0xe5e9f0 // brighter white
+    NORD6 = 0xeceff4 // brightest white
+
+    // FROST
+    NORD7 = 0x8fbcbb // turquoise
+    NORD8 = 0x88c0d0 // lightblue
+    NORD9 = 0x81a1c1 // semi darkblue
+    NORD10 = 0x5e81ac // darkblue
+
+    // AURORA
+    NORD11 = 0xbf616a // red
+    NORD12 = 0xd08770 // orange
+    NORD13 = 0xebcb8b // yellow
+    NORD14 = 0xa3be8c // green
+    NORD15 = 0xb48ead // purple
+)
